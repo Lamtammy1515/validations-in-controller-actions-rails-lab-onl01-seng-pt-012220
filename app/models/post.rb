d1baff0fe86db6f73e28f35f61ff11060ee80d86
Tammy Lam <lamtammy1515@gmail.com>
2020-06-15 02:48:38 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
-class Post < ActiveRecord::
+class Post < ActiveRecord::Base 
    validates :title, presence: true
   validates :content, length: { minimum: 100 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
